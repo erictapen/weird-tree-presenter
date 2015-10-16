74c415d37d9c7359cc4aead6c1971d854927d3f6
erictapen <justin.humm@posteo.de>
2015-10-16 14:57:39 +0200

    class GraphNode

--- src/main/java/GraphNode.java
+++ src/main/java/GraphNode.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+
 
 public class GraphNode {
-
+	private String caption;
+	private GraphNode parent;
+	private ArrayList<GraphNode> children;
+	private float size;
+	private float posx;
+	private float posy;
+	
+	public String getCaption() {
+		return caption;
+	}
+	public GraphNode getParent() {
+		return parent;
+	}
+	public ArrayList<GraphNode> getChildren() {
+		return children;
+	}
+	public float getSize() {
+		return size;
+	}
+	public float getPosx() {
+		return posx;
+	}
+	public float getPosy() {
+		return posy;
+	}
+	
+	
 }
